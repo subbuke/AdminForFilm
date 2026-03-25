709d0fe5772c0932afb98a1f0c3c9dfa5c9b3cf3
subramanyam <141896120+subbuke@users.noreply.github.com>
2026-03-25 06:06:17 +0000

    working

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,30 @@
- 
+ const express = require("express");
+ const app = express();
+ const mongoose = require("mongoose");
+const cors = require("cors")
+const Usermodel = require("./UserModel");
+
+app.use(cors());
+app.use(express.json())
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+    res.send("backend is running")
+})
+
+app.get("/data", (req, res) => {
+  Usermodel.find()
+  .then(data => res.json(data))
+  .catch(err => res.json(err))
+})
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("database connected"))
+.catch(err => console.log(err))
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log("server is running")
+})
